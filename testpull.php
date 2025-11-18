b8086af5f6814db22ae3aa35fcb0e4712bc9deb9
Vedder Timothy S. <veddertimothy@gmail.com>
2025-11-18 21:06:23 +0700

    Add initial HTML structure in testpull.php

---
+++ testpull.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tes Pull</title>
+</head>
+<body>
+    <h1>Sorr</h1>
+</body>
+</html>
