6a6ef55af38e8d3a527ddd873870156322d6ab76
Einleight <119738064+kamitsuki18384@users.noreply.github.com>
2025-12-05 21:30:57 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -4,10 +4,8 @@ $username = "root";
 $password = "";            
 $dbname = "parfum"; 
 
-// Buat koneksi
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Cek koneksi
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
